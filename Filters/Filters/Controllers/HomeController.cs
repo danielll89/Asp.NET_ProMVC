6d8be61861306373eb6cd39e4db333005eb50d5b
Daniel Jot <daniell.j89@gmail.com>
2017-05-22 22:49:02 +0200

    mvc filters

--- Filters/Filters/Controllers/HomeController.cs
+++ Filters/Filters/Controllers/HomeController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
+
     public class HomeController : Controller
     {
+        [Authorize(Users = "admin")]
         public string Index()
         {
             return "This is the Index action on the Home controller";
+        }
+
+        [GoogleAuth]
+        [Authorize(Users = "bob@google.com")]
+        public string List()
+        {
+            return "This is the List action on the Home controller";
+        }
+
+        [RangeException]
+        public string RangeTest(int id)
+        {
+            if (id > 100)
+            {
+                return String.Format("The id value is: {0}", id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
         }
     }
 }
